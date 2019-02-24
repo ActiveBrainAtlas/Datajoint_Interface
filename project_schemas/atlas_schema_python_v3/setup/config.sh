bfe66dc86a76bd4f4346f93e19a5b05bdf23d3f9
yoav freund <yfreund@ucsd.edu>
2019-02-24 15:43:07 -0800

    separated between config.sh and virtualize.sh

--- project_schemas/atlas_schema_python_v3/setup/config.sh
+++ project_schemas/atlas_schema_python_v3/setup/config.sh
@@ -35,24 +35,3 @@ echo ''
 export BUCKET_RAWDATA="mousebrainatlas-rawdata-backup"
 export BUCKET_DATA="mousebrainatlas-data"
 
-virtualenvdir="VirtEnv"
-##################################################
-## Defining colors ##
-red='\e[1;31m'
-purple='\e[1;35m'
-green='\e[1;32m'
-cyan='\e[1;36m'
-NC='\033[0m' # No Color
-## Done defining colors ##
-##################################################
-
-if [ ! -d $virtualenvdir ]; then
-    echo ""
-    echo -e "${red}Creating a virtual environment${NC}"
-    virtualenv -p python3 $ENV_DIR/$virtualenvdir
-fi
-
-source $ENV_DIR/$virtualenvdir/bin/activate
-
-pip3 install -r $ENV_DIR/setup/requirements.txt
-PATH=$PATH:$ENV_DIR/$virtualenvdir/lib/python3.6/site-packages/
