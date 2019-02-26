bdf305de52677c5c884f80b083001da0afd6e179
yoav freund <yfreund@ucsd.edu>
2019-02-25 18:46:30 -0800

    1234

--- project_schemas/atlas_schema_python_v3/Cell_Extractor/watchdog.py
+++ project_schemas/atlas_schema_python_v3/Cell_Extractor/watchdog.py
@@ -3,7 +3,7 @@
 from os.path import isfile,getmtime
 from glob import glob
 from time import sleep,time
-from os import system
+from os import system, environ
 from subprocess import Popen,PIPE
 import sys
 
@@ -20,25 +20,6 @@ local_data=params['paths']['data_dir']
 script='process_file.py'
 exec_dir=params['paths']['exec_dir']
 
-def runPipe(command):
-    print('cmd=',command)
-    p=Popen(command.split(),stdout=PIPE,stderr=PIPE)
-    L=p.communicate()
-    stdout=L[0].decode("utf-8").split('\n')
-    stderr=L[1].decode("utf-8").split('\n')
-    return stdout,stderr
-
-def run(command,out):
-    print('cmd=',command,'out=',out)
-    outfile=open(out,'w')
-    Popen(command.split(),stdout=outfile,stderr=outfile)
-
-def Last_Modified(file_name):
-    try:
-        mtime = getmtime(file_name)
-    except OSError:
-        mtime = 0
-    return(mtime)
 
 if __name__=='__main__':
     Recent=False
