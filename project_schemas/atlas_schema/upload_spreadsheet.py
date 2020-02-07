33988633a918e8a46fb9be777725b2daf5ee4663
eddyod <eddyod@yahoo.com>
2020-02-06 16:26:29 -0800

    Added a change to the fillna in upload

--- project_schemas/atlas_schema/upload_spreadsheet.py
+++ project_schemas/atlas_schema/upload_spreadsheet.py
@@ -1,3 +1,4 @@
+import argparse
 import yaml
 import pandas as pd
 from sqlalchemy import create_engine
@@ -35,7 +36,12 @@ def upload_spreadsheet(xlsx):
     # For each table in the spreadsheet
     for table_name, spreadsheet in spreadsheets.items():
         # Convert rows in the table to dicts
-        dict_rows = spreadsheet.fillna('').astype(str).to_dict(orient='records')
+        ##TODO, you can't fill in values with fillna('')
+        ## There is big difference between null values and empty values in sql
+        # https://stackoverflow.com/questions/42255474/pandas-dataframe-nan-values-replace-by-no-values
+        spreadsheet = spreadsheet.where(pd.notnull(spreadsheet), None)
+        #dict_rows = spreadsheet.fillna('').astype(str).to_dict(orient='records')
+        dict_rows = spreadsheet.to_dict(orient='records')
         for dict_row in dict_rows:
             # Create sqlalchemy instance and insert/update to the database
             new_row = tables[table_name.lower()]()
