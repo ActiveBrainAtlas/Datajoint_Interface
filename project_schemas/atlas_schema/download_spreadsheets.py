d65fb83be3333d68329ad58ff30e8f173822e422
Litao Qiao <checkpppp@gmail.com>
2020-02-12 12:23:04 -0800

    bug fixed for upload spreadsheet

--- project_schemas/atlas_schema/download_spreadsheets.py
+++ project_schemas/atlas_schema/download_spreadsheets.py
@@ -1,7 +1,4 @@
 import argparse
-import os
-
-PATH = os.path.dirname(os.path.abspath(__file__))
 
 parser = argparse.ArgumentParser(description='Download the tables for the specified animals. Each specified animal will generate one xlsx file. Each table is a seperate sheet in the file')
 parser.add_argument('animals', nargs='+', help='The animals to download.')
@@ -10,28 +7,8 @@ animals = args.animals
 
 import atlas_schema
 import pandas as pd
-import inspect
 from datetime import date
 from tqdm import tqdm
-
-def parse_definition(table):
-    definitions = []
-    
-    columns = [line for line in str(table.heading).split('\n') if ':' in line]
-    for column in columns:
-        definition = column.split('#')[0]
-        prefix, suffix = definition.split(':')
-        column_name = prefix.split('=')[0].strip()
-        
-        types = suffix.split('(')
-        column_type = types[0].strip()
-        if len(types) > 1:
-            column_value = types[1].strip()[:-1]
-        else:
-            column_value = None
-        
-        definitions.append((column_name, column_type, column_value))
-    return definitions
 
 def get_validates(definitions):
     TRANSFORM_TYPE = {
@@ -52,7 +29,7 @@ def get_validates(definitions):
     }
     
     validates = []
-    for column_name, column_type, column_value in definitions:
+    for column_name, column_default, column_type, column_value in definitions:
         validate = None
         if column_type in TRANSFORM_TYPE:
             validate = {}
@@ -67,24 +44,23 @@ def download_spreadsheet(animals):
     for prep_id in animals:
         print(f'Downloading the tables for {prep_id}', end='...')
         
-        writer = pd.ExcelWriter(PATH + '/' + prep_id + '.xlsx', engine='xlsxwriter')
+        writer = pd.ExcelWriter('./' + prep_id + '.xlsx', engine='xlsxwriter')
         workbook  = writer.book
         date_format = workbook.add_format({'num_format': 'yyyy-mm-dd'})
 
         # For each table definition
-        for table_name, table in tqdm(inspect.getmembers(atlas_schema, inspect.isclass)):
+        for table_name, table_class, table_defs in tqdm(atlas_schema.get_dj_tables()):
             # Write the data in current table to excel
             try:
-                rows = pd.DataFrame((table & ('prep_id = ' + f'"{prep_id}"')).fetch())
+                rows = pd.DataFrame((table_class & ('prep_id = ' + f'"{prep_id}"')).fetch())
             except: 
-                rows = pd.DataFrame(table.fetch())
+                rows = pd.DataFrame(table_class.fetch())
             rows.to_excel(writer, sheet_name=table_name, index=False)
 
             # Format the current spreadsheet
             worksheet = writer.sheets[table_name]
-            definitions = parse_definition(table)
-            validates = get_validates(definitions)
-            for index, ((column_name, column_type, column_value), validate) in enumerate(zip(definitions, validates)):
+            validates = get_validates(table_defs)
+            for index, ((column_name, _, column_type, _), validate) in enumerate(zip(table_defs, validates)):
                 # Calculate the letter for current column
                 column_letter = chr(ord('A') + index)
                 column_range = column_letter + '1:' + column_letter + '1048576'
@@ -100,6 +76,8 @@ def download_spreadsheet(animals):
                 if validate != None:
                     worksheet.data_validation(column_range, validate)
 
+        # Sort the spreadsheet in the provided order
+        workbook.worksheets_objs.sort(key=lambda x: atlas_schema.TABLE_NAMES.index(x.name))
         writer.save()
         print('Done')
         
